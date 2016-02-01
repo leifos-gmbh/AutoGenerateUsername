98801f2e6a046cc068a117a09c2d824f54794599
Fabian Wolf <wolf@leifos.com>
2016-02-01 11:13:10 +0100

    remove old patch file and change ilias min number

--- plugin.php
+++ plugin.php
@@ -4,11 +4,11 @@
 $id = "xagu";
 
 // code version; must be changed for all code changes
-$version = "1.1.1";
+$version = "1.1.2";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.0.4";
+$ilias_min_version = "5.1.0";
 $ilias_max_version = "5.1.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
