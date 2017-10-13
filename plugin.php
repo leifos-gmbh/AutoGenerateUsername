18a5c2125f013cb49af143654816eb1b85598456
xus <xus206@hotmail.com>
2017-10-13 15:27:50 +0200

    edit versions

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = "xagu";
 
 // code version; must be changed for all code changes
-$version = "5.2.1";
+$version = "5.3.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
